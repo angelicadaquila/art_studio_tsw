4bc0554ddc1ba93064d3eecc8c8494f8f080f9bc
angelicadaquila <angelicadaquila@gmail.com>
2026-08-04 17:35:53 +0200

    creato Javabean "RigaOrdine"

---
+++ artstudio/src/main/java/artstudio/model/RigaOrdine.java
@@ -0,0 +1,72 @@
+package artstudio.model;
+import java.io.Serializable;
+
+
+public class RigaOrdine implements Serializable{
+    private int idOrdine;
+    private int idProdotto;
+    private Double prezzoOg;
+    private int quantita;
+    private String descrizioneComm;
+    private String refComm;
+
+    public RigaOrdine() {
+    }
+
+    public RigaOrdine(int idOrdine, int idProdotto, Double prezzoOg, int quantita, String descrizioneComm, String refComm) {
+        this.idOrdine = idOrdine;
+        this.idProdotto = idProdotto;
+        this.prezzoOg = prezzoOg;
+        this.quantita = quantita;
+        this.descrizioneComm = descrizioneComm;
+        this.refComm = refComm;
+    }
+
+    public int getIdOrdine() {
+        return idOrdine;
+    }
+
+    public void setIdOrdine(int idOrdine) {
+        this.idOrdine = idOrdine;
+    }
+
+    public int getIdProdotto() {
+        return idProdotto;
+    }
+
+    public void setIdProdotto(int idProdotto) {
+        this.idProdotto = idProdotto;
+    }
+
+    public Double getPrezzoOg() {
+        return prezzoOg;
+    }
+
+    public void setPrezzoOg(Double prezzoOg) {
+        this.prezzoOg = prezzoOg;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
+    public String getDescrizioneComm() {
+        return descrizioneComm;
+    }
+
+    public void setDescrizioneComm(String descrizioneComm) {
+        this.descrizioneComm = descrizioneComm;
+    }
+
+    public String getRefComm() {
+        return refComm;
+    }
+
+    public void setRefComm(String refComm) {
+        this.refComm = refComm;
+    }
+}
