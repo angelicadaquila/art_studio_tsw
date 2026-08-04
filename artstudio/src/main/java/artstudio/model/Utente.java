a6780c0ec78030c9ea7cf39b5e09f9d05d63ae8d
angelicadaquila <angelicadaquila@gmail.com>
2026-08-04 17:36:24 +0200

    creato JavaBean "Utente"

---
+++ artstudio/src/main/java/artstudio/model/Utente.java
@@ -0,0 +1,62 @@
+package artstudio.model;
+import java.io.Serializable;
+
+public class Utente implements Serializable{
+    private int idUtente;
+    private String email;
+    private String password;
+    private String nome;
+    private String cognome;
+
+    
+    public Utente(){
+    }
+
+    public Utente(int idUtente, String email, String password, String nome, String cognome) {
+        this.idUtente = idUtente;
+        this.email = email;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public int getIdUtente() {
+        return idUtente;
+    }
+
+    public void setIdUtente(int idUtente) {
+        this.idUtente = idUtente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+}
