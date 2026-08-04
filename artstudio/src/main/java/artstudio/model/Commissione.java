3c0986b0666a63257f492f3f39051df08404c7b6
angelicadaquila <angelicadaquila@gmail.com>
2026-08-04 17:34:50 +0200

    creato Javabean "Commissione"

---
+++ artstudio/src/main/java/artstudio/model/Commissione.java
@@ -0,0 +1,21 @@
+package artstudio.model;
+
+public class Commissione extends Prodotto {
+    private String tempo;
+
+    public Commissione() {
+    }
+
+    public Commissione(int idProdotto, boolean isFisico, String nome, String descrizione, Double prezzo, boolean disponibile, String immagine, String tempo) {
+        super(idProdotto, isFisico, nome, descrizione, prezzo, disponibile, immagine);
+        this.tempo = tempo;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+}
