26e45998158ee453b6318b7e1b3aeeccb34e5a00
angelicadaquila <angelicadaquila@gmail.com>
2026-08-09 10:45:37 +0200

    creata interfaccia OrdineDAO

---
+++ artstudio/src/main/java/artstudio/dao/OrdineDAO.java
@@ -0,0 +1,25 @@
+package artstudio.dao;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+import artstudio.model.Ordine;
+
+public interface OrdineDAO {
+
+    public void doSave(Ordine ord) throws SQLException;
+
+    public Ordine doRetrieveByKey(int idOrdine) throws SQLException;
+
+    public List<Ordine> doRetrieveAll() throws SQLException;
+
+    public List<Ordine> doRetrieveByUtente(int idUtente) throws SQLException;
+    
+    public List<Ordine> doRetrieveByIntervalDate(Timestamp dataInizio, Timestamp dataFine) throws SQLException;
+    
+    public List<Ordine> doRetrieveByUtenteAndIntervalDate(int idUtente, Timestamp dataInizio, Timestamp dataFine) throws SQLException;
+    
+    public boolean doUpdateStato(int idOrdine, String nuovoStato) throws SQLException;
+
+    public boolean doDelete(int idOrdine) throws SQLException;
+}
